ad3bdd351cd8ec969702f342444e90c04d27e0d8
suryanarayanaraj <suryanarayanaraj16@gmail.com>
2026-03-06 22:31:49 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,75 +1,19 @@
-import { useState, useEffect } from "react";
-import TodoInput from "./components/TodoInput";
-import TodoItem from "./components/TodoItem";
-import FilterBar from "./components/FilterBar";
+import { useContext } from "react";
+import { ThemeContext } from "./context/ThemeContext";
+import ThemeToggle from "./components/ThemeToggle";
 import "./App.css";
 
 function App() {
 
-  const [tasks, setTasks] = useState([]);
-  const [filter, setFilter] = useState("all");
-
-  // Load tasks from localStorage
-  useEffect(() => {
-    const savedTasks = JSON.parse(localStorage.getItem("tasks"));
-    if (savedTasks) {
-      setTasks(savedTasks);
-    }
-  }, []);
-
-  // Save tasks to localStorage
-  useEffect(() => {
-    localStorage.setItem("tasks", JSON.stringify(tasks));
-  }, [tasks]);
-
-  const addTask = (text) => {
-    const newTask = {
-      id: Date.now(),
-      text: text,
-      completed: false
-    };
-
-    setTasks([...tasks, newTask]);
-  };
-
-  const toggleTask = (id) => {
-    setTasks(
-      tasks.map((task) =>
-        task.id === id ? { ...task, completed: !task.completed } : task
-      )
-    );
-  };
-
-  const deleteTask = (id) => {
-    setTasks(tasks.filter((task) => task.id !== id));
-  };
-
-  const filteredTasks = tasks.filter((task) => {
-    if (filter === "completed") return task.completed;
-    if (filter === "pending") return !task.completed;
-    return true;
-  });
+  const { theme } = useContext(ThemeContext);
 
   return (
-    <div className="app-container">
+    <div className={`app ${theme}`}>
+      <h1>Theme Switcher</h1>
 
-      <h1>To-Do App</h1>
+      <p>Toggle between Light and Dark mode</p>
 
-      <TodoInput addTask={addTask} />
-
-      <FilterBar setFilter={setFilter} />
-
-      <div className="todo-list">
-        {filteredTasks.map((task) => (
-          <TodoItem
-            key={task.id}
-            task={task}
-            toggleTask={toggleTask}
-            deleteTask={deleteTask}
-          />
-        ))}
-      </div>
-
+      <ThemeToggle />
     </div>
   );
 }

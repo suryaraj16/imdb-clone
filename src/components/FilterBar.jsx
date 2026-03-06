8fb42a0d2e9b6e42e71eb27aa45b6abc0c5900be
suryanarayanaraj <suryanarayanaraj16@gmail.com>
2026-03-06 22:26:26 +0530

    Create FilterBar.jsx

---
+++ src/components/FilterBar.jsx
@@ -0,0 +1,22 @@
+function FilterBar({ setFilter }) {
+
+  return (
+    <div className="filter-bar">
+
+      <button onClick={() => setFilter("all")}>
+        All
+      </button>
+
+      <button onClick={() => setFilter("completed")}>
+        Completed
+      </button>
+
+      <button onClick={() => setFilter("pending")}>
+        Pending
+      </button>
+
+    </div>
+  );
+}
+
+export default FilterBar;

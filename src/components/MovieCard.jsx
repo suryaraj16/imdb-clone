0d1b98b01bbefb17d1957e943a17a9db6ec3dda4
suryanarayanaraj <suryanarayanaraj16@gmail.com>
2026-03-06 22:14:33 +0530

    Create MovieCard.jsx

---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,11 @@
+function MovieCard({ movie }) {
+  return (
+    <div className="movie-card">
+      <img src={movie.Poster} alt={movie.Title} />
+      <h3>{movie.Title}</h3>
+      <p>{movie.Year}</p>
+    </div>
+  );
+}
+
+export default MovieCard;

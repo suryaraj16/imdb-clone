fe0adca93e6f209ac551bfe4ccbabfb8edbfbea6
suryanarayanaraj <suryanarayanaraj16@gmail.com>
2026-03-06 22:43:00 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,11 +1,13 @@
-const API_KEY = "YOUR_API_KEY";
+const API_KEY = "f5ea337c";
 const BASE_URL = "https://www.omdbapi.com/";
 
 export const searchMovies = async (query) => {
+
   const response = await fetch(
     `${BASE_URL}?s=${query}&apikey=${API_KEY}`
   );
 
   const data = await response.json();
-  return data.Search;
+
+  return data.Search || [];
 };

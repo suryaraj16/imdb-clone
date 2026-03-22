913b70fdb983f551ebce2bf9c71d20d4791f50c5
suryanarayanaraj <suryanarayanaraj16@gmail.com>
2026-03-22 13:35:24 +0530

    Update Reviews.jsx

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -1,1 +1,36 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
 
+export default function Reviews({ movieId }) {
+  const [data, setData] = useState({
+    reviews: [],
+    avgRating: 0,
+  });
+
+  const fetchReviews = async () => {
+    const res = await axios.get(
+      `http://localhost:5000/api/reviews/${movieId}`
+    );
+    setData(res.data);
+  };
+
+  useEffect(() => {
+    fetchReviews();
+  }, [movieId]);
+
+  return (
+    <div className="mt-4">
+      <h2>⭐ Avg Rating: {data.avgRating}</h2>
+
+      {data.reviews.map((r) => (
+        <div key={r._id} className="border p-2 mt-2">
+          <strong>{r.username}</strong> - ⭐ {r.rating}
+          <p>{r.comment}</p>
+          <small>
+            {new Date(r.createdAt).toLocaleString()}
+          </small>
+        </div>
+      ))}
+    </div>
+  );
+}

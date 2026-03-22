6017c76505d2a6de30dcff538f9f77b46655ef8e
suryanarayanaraj <suryanarayanaraj16@gmail.com>
2026-03-22 13:33:05 +0530

    Update Review.js

--- backend/models/Review.js
+++ backend/models/Review.js
@@ -1,1 +1,21 @@
+const mongoose = require("mongoose");
 
+const reviewSchema = new mongoose.Schema(
+  {
+    movieId: {
+      type: String,
+      required: true,
+    },
+    username: String,
+    rating: {
+      type: Number,
+      min: 1,
+      max: 10,
+      required: true,
+    },
+    comment: String,
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Review", reviewSchema);
